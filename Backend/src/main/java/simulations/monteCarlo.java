a4dd29d182addf7888c51e791d899f56c3eef9f5
HSK <haakonsk@live.no>
2025-02-11 09:20:27 +0100

    Basic Monte Carlo Simulation works

--- Backend/src/main/java/simulations/monteCarlo.java
+++ Backend/src/main/java/simulations/monteCarlo.java
@@ -1,5 +1,7 @@
 package simulations;
 
+import java.util.Random;
+
 public class monteCarlo {
   /**
    * Monte Carlo Simulation of a warehouse time to complete all tasks within a day
@@ -11,6 +13,32 @@ public class monteCarlo {
    */
   public static void main(String[] args) {
 
+    int simCount = 10000; // Number of simulations
+    int minWorkers = 8, maxWorkers = 10; // Number of workers active on a given day as a range
 
+    int minTasks = 10, maxTasks = 100; // Number of tasks on a given day as a range
+    int minTaskTime = 5, maxTaskTime = 30; // Time to complete each task as a range
+
+
+    Random random = new Random();
+    double totalCompletionTime = 0;
+
+    for (int i = 0; i < simCount; i++) {
+      int tasks = random.nextInt(maxTasks - minTasks + 1) + minTasks;
+      int workers = random.nextInt(maxWorkers - minWorkers + 1) + minWorkers;
+
+      double totalTaskTime = 0;
+        for (int j = 0; j < tasks; j++) {
+            totalTaskTime += random.nextInt(maxTaskTime - minTaskTime + 1) + minTaskTime;
+        }
+
+        double completionTime = totalTaskTime / workers; // For each worker the time to complete a task is reduced
+        totalCompletionTime += completionTime;
+      // System.out.println("Simulation " + (i + 1) + ": " + completionTime);
+    }
+
+    double averageCompletionTime = totalCompletionTime / simCount;
+    System.out.println("Average time to complete all tasks: " + averageCompletionTime);
   }
+
 }
