d8cfa104d05c21040e8764827f286deed0a67245
Adrian Johansen <adrfaujoh@hotmail.com>
2025-02-11 08:27:01 +0100

    Added springboot backend

---
+++ Backend/src/main/java/gruppe01/ntnu/no/Warehouse/Workflow/Assigner/WarehouseWorkflowAssignerApplication.java
@@ -0,0 +1,13 @@
+package gruppe01.ntnu.no.Warehouse.Workflow.Assigner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseWorkflowAssignerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseWorkflowAssignerApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/gruppe01/ntnu/no/Warehouse/Workflow/Assigner/WarehouseWorkflowAssignerApplicationTests.java
@@ -0,0 +1,13 @@
+package gruppe01.ntnu.no.Warehouse.Workflow.Assigner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseWorkflowAssignerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
