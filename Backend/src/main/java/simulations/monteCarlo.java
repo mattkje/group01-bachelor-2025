8165cdcb8426e3c260eb3467c41854d9756e969d
HSK <haakonsk@live.no>
2025-02-11 08:55:05 +0100

    Added simulations folder with a montecarlo class

---
+++ Backend/src/main/java/simulations/monteCarlo.java
@@ -0,0 +1,16 @@
+package simulations;
+
+public class monteCarlo {
+  /**
+   * Monte Carlo Simulation of a warehouse time to complete all tasks within a day
+   *
+   * Currently takes into account the following:
+   * - Number of tasks
+   * - Time to complete each task
+   * - Number of workers
+   */
+  public static void main(String[] args) {
+
+
+  }
+}
