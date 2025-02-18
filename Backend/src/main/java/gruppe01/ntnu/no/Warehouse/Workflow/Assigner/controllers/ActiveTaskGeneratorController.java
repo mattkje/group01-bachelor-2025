7e36d8594d6e27a2e63111f18eb5147450a5c3b3
Adrian Johansen <adrfaujoh@hotmail.com>
2025-02-18 12:49:09 +0100

    zzzz

--- Backend/src/main/java/gruppe01/ntnu/no/Warehouse/Workflow/Assigner/controllers/ActiveTaskGeneratorController.java
+++ Backend/src/main/java/gruppe01/ntnu/no/Warehouse/Workflow/Assigner/controllers/ActiveTaskGeneratorController.java
@@ -2,6 +2,7 @@ package gruppe01.ntnu.no.Warehouse.Workflow.Assigner.controllers;
 
 import gruppe01.ntnu.no.Warehouse.Workflow.Assigner.dummydata.ActiveTaskGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ public class ActiveTaskGeneratorController {
     @Autowired
     private ActiveTaskGenerator activeTaskGeneratorService;
 
-    @PostMapping("/generate")
+    @GetMapping("/generate")
     public void generateActiveTasks() throws Exception {
         activeTaskGeneratorService.generateActiveTasks();
     }
