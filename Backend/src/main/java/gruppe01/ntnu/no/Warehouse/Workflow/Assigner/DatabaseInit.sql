69aaea3ebed424b4960923d52449d56e45a0a201
Adrian Johansen <adrfaujoh@hotmail.com>
2025-02-18 12:56:09 +0100

    Fix to database error

--- Backend/src/main/java/gruppe01/ntnu/no/Warehouse/Workflow/Assigner/DatabaseInit.sql
+++ Backend/src/main/java/gruppe01/ntnu/no/Warehouse/Workflow/Assigner/DatabaseInit.sql
@@ -54,7 +54,7 @@ CREATE TABLE IF NOT EXISTS active_task
     due_date   TIMESTAMP,
     start_time TIMESTAMP,
     end_time   TIMESTAMP,
-    eta        TIMESTAMP DEFAULT NULL,
+    eta        DATETIME(6) DEFAULT NULL,
     FOREIGN KEY (task_id) REFERENCES task (id)
 );
 
