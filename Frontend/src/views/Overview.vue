866141c5c7548bd2287f0312b44db83fd909e97d
Matti Kjellstadli <40216910+mattkje@users.noreply.github.com>
2025-03-24 12:51:07 +0100

    Small change

--- Frontend/src/views/Overview.vue
+++ Frontend/src/views/Overview.vue
@@ -1,17 +1,28 @@
 <template>
   <Toolbar title="Overview" />
   <div class="overview">
-    <p>There is no overview right now...</p>
+    <pre>{{ asciiArt }}</pre>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import { ref, onMounted } from 'vue';
 import Toolbar from "@/components/Toolbar.vue";
+import AsciiArt from "@/assets/ascii.txt";
+
+const asciiArt = ref('');
+
+onMounted(async () => {
+  const response = await fetch(AsciiArt);
+  asciiArt.value = await response.text();
+});
 </script>
 
 <style scoped>
 .overview {
   padding: 2rem;
+  white-space: pre-wrap;
+  font-size: 0.4rem;
+  overflow-y: auto;
 }
 </style>
