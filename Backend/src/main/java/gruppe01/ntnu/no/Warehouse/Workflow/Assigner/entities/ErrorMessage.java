5fc404bc9c957604f5dc7bac4faf08a4e0937ede
Adrian Johansen <adrfaujoh@hotmail.com>
2025-05-15 08:50:23 +0200

    added table tags

--- Backend/src/main/java/gruppe01/ntnu/no/Warehouse/Workflow/Assigner/entities/ErrorMessage.java
+++ Backend/src/main/java/gruppe01/ntnu/no/Warehouse/Workflow/Assigner/entities/ErrorMessage.java
@@ -1,21 +1,29 @@
 package gruppe01.ntnu.no.Warehouse.Workflow.Assigner.entities;
 
+        import io.swagger.v3.oas.annotations.media.Schema;
         import jakarta.persistence.*;
 
         import java.time.LocalDateTime;
 
         @Entity
+        @Table(name = "error_message")
+        @Schema(description = "Represents an error message entity in the system.")
         public class ErrorMessage {
 
             @Id
             @GeneratedValue(strategy = GenerationType.IDENTITY)
             private long id;
 
-            @Column(columnDefinition = "LONGTEXT")
+            @Column(name = "message", columnDefinition = "LONGTEXT")
+            @Schema(description = "The error message.")
             private String message;
 
+            @Column(name = "zone_id")
+            @Schema(description = "The ID of the zone where the error occurred.")
             private long zoneId;
 
+            @Column(name = "time")
+            @Schema(description = "The time when the error occurred.")
             private LocalDateTime time;
 
             public ErrorMessage() {
